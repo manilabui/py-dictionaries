efee3b26d118baa61c2a00b1b52ad31c606386cd
Manila Bui <manila.bui@gmail.com>
2020-01-14 09:25:42 -0600

    finished words + started idioms

---
+++ dictionaries.py
@@ -0,0 +1,52 @@
+# words
+"""
+Create a dictionary with key value pairs to
+represent words (key) and its definition (value)
+"""
+word_definitions = {
+		"word1": "def1",
+		"word2": "def2",
+		"word3": "def3",
+		"word4": "def4"
+}
+
+"""
+Add several more words and their definitions
+   Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+"""
+
+"""
+Use square bracket lookup to get the definition of two
+words and output them to the console with `print()`
+"""
+
+print(word_definitions["word1"])
+print(word_definitions["word1"])
+
+"""
+Loop over the dictionary to get the following output:
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+"""
+
+for word in word_definitions:
+		print(f'The definition of {word} is {word_definitions[word]}')
+
+#idioms
+idioms = {
+    "Penny": ["A", "penny", "for", "your", "thoughts"],
+    "Injury": ["Add", "insult", "to", "injury"],
+    "Moon": ["Once", "in", "a", "blue", "moon"],
+    "Grape": ["I", "heard", "it", "through", "the", "grapevine"],
+    "Murder": ["Kill", "two", "birds", "with", "one", "stone"],
+    "Limbs": ["It", "costs", "an", "arm", "and", "a", "leg"],
+    "Grain": ["Take", "what", "someone", "says", "with", "a", "grain", "of", "salt"],
+    "Fences": ["I'm", "on", "the", "fence", "about", "it"],
+    "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
+    "Lucifer": ["Speak", "of", "the", "devil"],
+}
+
+for idiom_title in idioms:
+		idiom 
+		print(f'{idiom_title}')
